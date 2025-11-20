94278866cd44e6d206533f63333d0b02d186a72e
DiligentViking <acore-n@proton.me>
2025-11-20 15:14:54 -0600

    Add basic list and todo functions to model

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,4 +2,8 @@ import { createListModel } from './model/model.js';
 
 const model = createListModel();
 
-model.addProject('QWER');
+model.addList('QWER');
+
+model.addTodo('QWER', 'Do the campaign', 'Make sure you master the lessons it teaches');
+
+model.markTodoAsDone('QWER', 0);
--- src/scripts/model/model.js
+++ src/scripts/model/model.js
@@ -1,14 +1,65 @@
 export function createListModel() {
   const lists = {};
 
-  const addProject = (name) => {
-    if (lists[name]) {
-      console.error('Project already exists');
-      return;
+  const showData = () => console.log(lists);
+
+  const checkListExistence = (listName) => {
+    if (!lists[listName]) {
+      console.error('List does not exist');
+      return false;
     }
-    lists[name] = {todos: []};
-    console.log(lists);
+    return true;
   }
 
-  return {addProject};
+  const updateTodo = (listName, todoID, prop, val) => {
+    lists[listName].todos[todoID][prop] = val;
+  }
+
+  return {
+    addList(listName) {
+      if (lists[listName]) {
+        console.error('List already exists');
+        return;
+      }
+
+      lists[listName] = {todos: []};
+      showData();
+    },
+
+
+    deleteList(listName) {
+      if (lists[listName].todos.length) {
+        console.error('List must be empty before deletion');
+        return;
+      }
+
+      delete lists[listName];
+      showData();
+    },
+
+
+    // Todo Funcs //
+
+    addTodo(listName, title, notes, priority, datetimedue, isDone=null) {
+      if (!checkListExistence(listName)) return;
+
+      lists[listName].todos.push({title, notes, priority, datetimedue, isDone});
+      showData();
+    },
+
+
+    markTodoAsDone(listName, todoID) {
+      if (!checkListExistence(listName)) return;
+
+      updateTodo(listName, todoID, 'isDone', 1);
+      showData();
+    },
+
+    markTodoAsNotDone(listName, todoID) {
+      if (!checkListExistence(listName)) return;
+
+      updateTodo(listName, todoID, 'isDone', null);
+      showData();
+    },
+  };
 }
